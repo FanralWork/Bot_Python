e67fa56f6f44bbe3b10547d1127f8b8993e44c24
Фомичев Дмитрий <d.fomichev@kvantorium62.local>
2020-10-17 15:36:49 +0300

    add_info

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import telebot
 bot = telebot.TeleBot('1345627871:AAEo4OD-AhQCDWS77DUo_-P7VUz2ZsuXDuo')
 
-
-
 @bot.message_handler(commands=["start"])
 def handle_text(message):
-    bot.send_message(message.chat.id, """Привет я Хоттабыч. Я могу помочь тебе купить игры со скидкой, 
+    bot.send_message(message.chat.id, """Привет я Хоттабыч. Я могу помочь тебе купить игры со скидкой,
 чтобы узнать мои команды введи /help""")
 #Приветствие#
 
+@bot.message_handler(commands=["info"])
+def handle_text(message):
+    bot.send_message(message.chat.id, """Версия бота:
+/games - Купить игры
+/programs - купить программы""")
+#Все команды#
+
 @bot.message_handler(commands=["help"])
 def handle_text(message):
-    bot.send_message(message.chat.id, """Мои команды: 
+    bot.send_message(message.chat.id, """Мои команды:
 /games - Купить игры
 /programs - купить программы""")
 #Все команды#
