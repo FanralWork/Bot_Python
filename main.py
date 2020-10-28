2b7840e5af62fb69181cb61e312645942c623729
Фомичев Дмитрий <d.fomichev@kvantorium62.local>
2020-10-28 17:19:40 +0300

    15

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+basket = 0
 
 bot = telebot.TeleBot('1345627871:AAEo4OD-AhQCDWS77DUo_-P7VUz2ZsuXDuo')
 
@@ -8,12 +9,17 @@ def heandle_start(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
     user_markup.row("/start", "/stop")
     user_markup.row("/help", "/info")
-    user_markup.row("/games") #"/programs"
+    user_markup.row("/games", "/basket")
     bot.send_message(message.chat.id, """Привет я Хоттабыч. Я могу помочь тебе купить игры со скидкой,
 чтобы узнать мои команды введи /help""", reply_markup=user_markup)
     bot.send_photo(message.chat.id, 'http://memok.net/uploads/2015/10/24/562c19b19826a.jpg', reply_markup=user_markup)
 
 # Приветствие#
+
+@bot.message_handler(commands=["basket"])
+def heandle_stop(message):
+    remove_markup = telebot.types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, basket, reply_markup=remove_markup)
 
 @bot.message_handler(commands=["stop"])
 def heandle_stop(message):
@@ -32,7 +38,11 @@ def handle_text(message):
 def handle_text(message):
     bot.send_message(message.chat.id, """Мои команды:
 /games - Купить игры
-/programs - купить программы""")
+/start - Начать работу
+/stop - Закончить работу с ботом
+/info - Информация о версии бота""")
+#/programs - купить программы#
+
 
 
 # Все команды#
@@ -46,11 +56,27 @@ def handle_text(message):
     item2 = types.InlineKeyboardButton("Перейти в магазин", callback_data='Link_GTA_5')
     markup.add(item1, item2)
     bot.send_message(message.chat.id, text="GTA 5 - 645 рублей", reply_markup=markup)
+    #GTA 5#
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    item1 = types.InlineKeyboardButton("Купить", callback_data='Link_Buy_Fallout_76')
+    item2 = types.InlineKeyboardButton("Перейти в магазин", callback_data='Link_GTA_5')
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, text="Fallout 76 - 479 рублей", reply_markup=markup)
+    #Fallout 76#
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    item1 = types.InlineKeyboardButton("Купить", callback_data='Link_Buy_PUBG')
+    item2 = types.InlineKeyboardButton("Перейти в магазин", callback_data='Link_GTA_5')
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, text="PUBG - 379 рублей", reply_markup=markup)
+    #PUBG#
 
     markup = types.InlineKeyboardMarkup(row_width=1)
     item1 = types.InlineKeyboardButton("Перейти в магазин", callback_data='Link_GTA_5')
     markup.add(item1)
     bot.send_message(message.chat.id, text="Чтобы увидеть ещё игры прейди на сайт.", reply_markup=markup)
+    #Переход на сайт#
 
 
 # Игры#
@@ -83,9 +109,15 @@ def callbackInline(call):
             elif call.data == 'Link_GTA_5':
                 bot.send_message(call.message.chat.id, "https://zaka-zaka.com/?agent=#1965376")
 
-                # remove inline buttons
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      text="Чтобы увидеть ещё игры прейди на сайт.",
+            elif call.data == "Link_Buy_Fallout_76":
+                bot.send_message(call.message.chat.id,
+                                 "https://zaka-zaka.com/game/fallout-76/?agent=#1965376")
+            elif call.data == "Link_Buy_PUBG":
+                bot.send_message(call.message.chat.id,
+                                 "https://zaka-zaka.com/game/playerunknowns-battlegrounds/?agent=#1965376")
+            # remove inline buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text="Чтобы купить игру прейди на сайт.",
                                       reply_markup=None)
     except Exception as e:
         print(repr(e))
